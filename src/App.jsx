dc67f48bc248a632b1da7f030389fa88429f0331
sansika <it21028014@my.sliit.lk>
2024-07-02 17:36:23 +0530

    Initialization of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "./app.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Test from "./components/Test";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    {/* <section><Navbar/></section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Portfolio</section>
+    <section>Contact</section> */}
+   <Test/>
+  </div>;
 };
 
 export default App;
