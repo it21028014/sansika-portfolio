e1bb564990cac9d391a47b9fab1df71c1064f282
sansika <it21028014@my.sliit.lk>
2024-07-04 17:10:53 +0530

    upfated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./app.scss";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 
 const App = () => {
   return <div>
-    <section id="Homepage"><Navbar/></section>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+      </section>
     <section id="Services">Parallax</section>
     <section id="Porfolio">Services</section>
     <section >Portfolio</section>
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+       
+    </div>
+  )
+}
+
+export default Hero
+ 
