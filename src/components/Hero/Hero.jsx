a6c7e83131dc000a37aa238453e4b96351d6870a
Sansika Kodithuwakku <sansikakodi@Sansikas-MacBook-Pro.local>
2024-07-07 20:40:04 +0530

    Hero partially done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,20 @@ import './Hero.scss'
 const Hero = () => {
   return (
     <div className='hero'>
-       
+    <div className="wrapper">
+      <div className="textContainer">
+        <h2>SANSIKA KODITHUWAKKU</h2>
+        <h1>Full Stack Web And Mobile Developer</h1>
+        <div className="buttons">
+          <button>See latest Works</button>
+          <button>Contact Me</button>
+        </div>
+        <img src='/scroll.png'/>
+      </div>
+    </div>
+    <div className="imageContainer">
+       <img src='/hero.png' alt='' />
+    </div>
     </div>
   )
 }
