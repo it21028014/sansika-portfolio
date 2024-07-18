dbf721809068702691cc1f5a9686002959a0b415
it21028014@my.sliit.lk <it21028014@my.sliit.lk>
2024-07-18 17:25:57 +0530

    Services scss bug fixes needs to to do

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import "./Services.scss";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Services = () => {
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      animate="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
         <p>
           I focus on helping your brand grow
           <br /> and move forward
         </p>
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="/people.webp" />
           <h1>
@@ -19,22 +41,77 @@ const Services = () => {
           </h1>
         </div>
         <div className="title">
-          <img src="/people.webp" />
           <h1>
             <b>For Your</b> Business.
           </h1>
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
-        <div className="box">
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
           <h2>Branding</h2>
           <p>
-            ;lrwg rwgl;g wg rthe t rthe g rwg w wgw gwr gwd sr gsdg rh ehsr f h
-            sfhsfghlwrknglr;kghwrg wrghlw rg;slrk 00
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, PageMaker including versions of Lorem Ipsum.
           </p>
           <button>Go</button>
-        </div>
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button>Go</button>
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button>Go</button>
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button>Go</button>
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button>Go</button>
+        </motion.div>
       </motion.div>
     </motion.div>
   );
